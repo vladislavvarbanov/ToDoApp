6a68dde7b89b4f81c27f1544276ae85282b3cbe5
vladislavvarbanov <vladislavvarbanov9@gmail.com>
2025-07-10 11:16:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 import './App.css';
 
 function App() {
